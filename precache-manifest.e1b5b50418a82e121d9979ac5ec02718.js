3c5c474f14fafa415764718fc3c52cdec77f4930
engrnonny <engr.nonny@gmail.com>
2024-04-30 01:14:07 +0100

    First commit

---
+++ precache-manifest.e1b5b50418a82e121d9979ac5ec02718.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590f35d9a2678f60c2fb32d8b1236ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06cf18db60e813c6bc90",
+    "url": "/static/css/main.4a588f39.chunk.css"
+  },
+  {
+    "revision": "dadc35a0c3c0c6dc9d1a",
+    "url": "/static/js/2.16732f86.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.16732f86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8751ece060d2c676e7e0",
+    "url": "/static/js/3.b75b75f3.chunk.js"
+  },
+  {
+    "revision": "06cf18db60e813c6bc90",
+    "url": "/static/js/main.caaf25c3.chunk.js"
+  },
+  {
+    "revision": "f97249719c5ac267fbe5",
+    "url": "/static/js/runtime-main.50c050eb.js"
+  }
+]);
